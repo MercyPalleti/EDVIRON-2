2f94bdcbc5e1534d56ddb30dde3510aa6a5a56ce
MercyPalleti <palletimercy1@gmail.com>
2025-09-15 23:25:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 //app.use(cors());
 app.use(morgan('dev'));
-const allowedOrigin = 'https://edviron-frontend-d2fci2fvp-mercys-projects-f7f4a381.vercel.app';
+const allowedOrigin = 'https://edviron-frontend-livid.vercel.app';
 
 app.use(cors({
   origin: allowedOrigin,
